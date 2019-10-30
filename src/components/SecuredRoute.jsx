10f693b2fcda1ae8cd46d013829c7e880d481193
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2019-10-30 20:31:42 +0800

    Added Page Loading Animation

--- src/components/SecuredRoute.jsx
+++ src/components/SecuredRoute.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import auth0Client from './Auth';
+import Spinner from 'react-bootstrap/Spinner';
 
 function SecuredRoute(props) {
     const {component: Component, path, checkingSession, ...rest} = props;
+    const loadingAnimation = (
+        <Spinner animation="grow"/>
+    );
     return (
         <Route {...rest} path={path} render={properties => {
-            if (checkingSession) return <h3 className="text-center">Validating session...</h3>;
+            if (checkingSession) return loadingAnimation;
             if (!auth0Client.isAuthenticated()) {
                 auth0Client.signIn();
                 return <div/>;
